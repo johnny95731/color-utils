92f5d2124520f866553fe9355c28aa7269fd9006
johnny95731 <johnny95731@yahoo.com>
2025-05-29 07:31:47 +0800

    test: print markdown table format in performanceTest

--- test-utils/perf.js
+++ test-utils/perf.js
@@ -4,6 +4,53 @@ const round = (val, place = 0) => {
   return Math.round(10**place * val) / 10**place;
 };
 
+function convertToMarkdownTable(data) {
+  if (!data || data.length === 0) {
+    return '';
+  }
+
+  const keys = ['Task name', 'Latency avg (ns)', 'Throughput avg (ops/s)', 'Comparison'];
+  const header = ['Library', 'Latency avg (ns)', 'Throughput avg (ops/s)', 'Comparison'];
+
+  const maxTextLength = keys.map(() => 0);
+  const markdownData = data.map((obj) => {
+    const ret = {};
+    keys.forEach((key, i) => {
+      const text = obj[key];
+      let len;
+      if (key !== 'Comparison' && key !== 'Task name') {
+        const arr = text.split('±');
+        len = arr[0].length;
+      } else len = text.length;
+      if (len > maxTextLength[i]) maxTextLength[i] = len;
+      ret[key] = text;
+    });
+    return ret;
+  });
+
+  markdownData.forEach(obj => {
+    keys.forEach((key, j) => {
+      if (j === 0) {
+        obj[key] = obj[key].padEnd(maxTextLength[0]);
+      } else if (key !== 'Comparison') {
+        const arr = obj[key].split('±');
+        const numPadding = maxTextLength[j] - arr[0].length;
+        arr[0] += '&ensp;'.repeat(numPadding);
+        obj[key] = arr.join('±');
+      }
+    });
+  });
+
+  let markdown = ` ${header.join(' | ')}\n`;
+  markdown += `${header.map(text => ''.padEnd(text.length + 2, '-')).join('|')}\n`;
+
+  for (const row of markdownData) {
+    const rowValues = keys.map(header => row[header]);
+    markdown += `${rowValues.join(' | ')}\n`;
+  }
+  return markdown;
+}
+
 /**
  * @param {string} testName
  * @param {(CallableFunction | [string, CallableFunction])[]} fns
@@ -48,10 +95,13 @@ export const performanceTest = async (
       }
     });
     table.forEach((t, i) => {
-      t['comparison'] = comparison[i];
+      t['Comparison'] = comparison[i];
     });
 
     console.table(table);
+
+    const markdowns = convertToMarkdownTable(table);
+    console.table(markdowns);
   } else {
     console.log(testName, 'completed.');
   }
