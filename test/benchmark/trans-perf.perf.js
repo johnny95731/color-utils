bdb7a8bf5f2737a320ecd6d9b813f4638401c0be
johnny95731 <johnny95731@yahoo.com>
2025-05-29 10:54:18 +0800

    refactor: reorder files

--- test/benchmark/trans-perf.perf.js
+++ test/benchmark/trans-perf.perf.js
@@ -3,12 +3,11 @@
  */
 
 // Controll the test samples.
-// import { SampleGenerator } from '../test-utils/sample.js';
-// SampleGenerator.b(5);
+// import { SampleGenerator } from '../../test-utils/sample.js';
+// SampleGenerator.b(10);
 
 await import('./colorModels/ciexyz.perf.js');
 await import('./colorModels/cielab.perf.js');
-await import('./colorModels/oklab.perf.js');
 await import('./colorModels/cieluv.perf.js');
 await import('./colorModels/cmyk.perf.js');
 await import('./colorModels/hex.perf.js');
@@ -16,3 +15,4 @@ await import('./colorModels/hsb.perf.js');
 await import('./colorModels/hsl.perf.js');
 await import('./colorModels/hwb.perf.js');
 await import('./colorModels/named.perf.js');
+await import('./colorModels/oklab.perf.js');
